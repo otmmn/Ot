5057fcce39b87ede136114b00d8c071f0d053772
Othmane RAZZOUQ <razzouq.othmane@gmail.com>
2026-03-01 22:22:13 +0000

    Motivational quotes included

--- src/Config/Quotes.js
+++ src/Config/Quotes.js
@@ -2,5 +2,12 @@ export const Quotes = {
   timeQuotes: [
     "Not to scare you but time really is the only determining factor",
     "Testing this funny time quote",
+    "Every second counts",
+  ],
+
+  motivationalQuotes: [
+    "The saga begins",
+    "Call me the specialist professional professor",
+    "G.O.D, father pt. 3, QBC sip lime McArthy",
   ],
 };
