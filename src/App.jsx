c9ec8110b876813cd531abe5dac9b84a8fdef336
Othmane RAZZOUQ <razzouq.othmane@gmail.com>
2026-03-11 00:41:04 +0000

    Shadcn ui integrated Components folder redefined to include shadcn ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header/Header";
 import { Panel } from "./components/Panel/Panel";
 import { DisplayTasks } from "./components/Tasks/DisplayTasks";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
